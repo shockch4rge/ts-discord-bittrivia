c5b5bd4b2fe66a8fff572e14123f9cb4c0dd9c5e
Shockch4rge <favouriteteo@gmail.com>
2021-10-08 03:05:43 +0800

    Separate MessageController and MainController

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { Client, Intents} from 'discord.js'
-import {MainController} from "./MainController";
+import MainController from "./controllers/MainController";
 import { BOT_TOKEN } from "./auth.json";
 
 const bot = new Client({intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]});
 const mainController = new MainController(bot);
 
-mainController.subscribeBotEvents();
+mainController.subscribeBotEvents(bot);
 
 void bot.login(BOT_TOKEN);
