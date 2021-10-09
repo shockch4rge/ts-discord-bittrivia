aad02a642d5354df7e46ad817138ec45fb067745
Shockch4rge <favouriteteo@gmail.com>
2021-10-09 17:13:45 +0800

    Allow anyone to respond to question

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -1,7 +1,8 @@
 import {Message} from "discord.js";
 import Question from "../models/Question";
-import {deleteMessages, editMessage, MessageLevel, sendMessage} from "../../utils/messaging";
+import {deleteMessages, editMessage, MessageLevel} from "../utils/messaging";
 import ApiService from "../services/ApiService";
+import {createEmbed} from "../utils/utils";
 
 export default class MessageController {
     // we use a message lock to prevent message events from firing while
@@ -15,14 +16,10 @@ export default class MessageController {
     }
 
     public async handleMessageCreate(message: Message) {
-        if (message.channelId !== "879645377463222313") return;
         if (message.author.bot || !message.guild || !message.channel) return;
-        if (this.messageLock) {
-            console.log("Message lock is active");
-            return;
-        }
+        if (this.messageLock) return;
 
-        // 50% chance of question popping up with every message
+        // 1% chance of question popping up with every message
         const chance = 0.5
 
         if (chance >= Math.random()) {
@@ -39,25 +36,24 @@ export default class MessageController {
 
         // construct the question and send it to the channel
         const question = new Question(questionData!);
-        const questionMessage = await sendMessage(message.channel, {
+        const questionEmbed = createEmbed({
             title: question.content,
             fields: [
                 {name: "Category", value: question.category, inline: true},
                 {name: "Type", value: question.type, inline: true},
                 {name: "Difficulty", value: question.difficulty, inline: true},
-                {name: "1", value: question.allAnswers[0]},
-                {name: "2", value: question.allAnswers[1], inline: true},
-                {name: "3", value: question.allAnswers[2]},
-                {name: "4", value: question.allAnswers[3], inline: true},
             ],
             footer: "❗ There is only 1 correct answer."
         });
 
-        // filter messages from original author
-        const f = (m: Message): boolean => m.author.id === message.author.id;
+        for (let i = 1; i < question.allAnswers.length + 1; i++) {
+            questionEmbed.addField(i.toString(), question.allAnswers[i - 1]);
+        }
+
+        const questionMessage = await message.channel.send({embeds: [questionEmbed]});
 
         try {
-            const answers = await message.channel.awaitMessages({filter: f, max: 1, time: 8000, errors: ['time']});
+            const answers = await message.channel.awaitMessages({max: 1, time: 15000, errors: ['time']});
 
             // correct answer
             if (!question.checkCorrect(answers.first()!.content)) {
@@ -66,23 +62,30 @@ export default class MessageController {
                     level: MessageLevel.WARNING,
                     footer: `Correct answer: ${question.correctAnswer}`,
                 });
-                await deleteMessages([alertWrong, answers.first()!], 5000);
+                await deleteMessages([alertWrong], 5000);
             }
             // wrong answer
             else {
                 const alertCorrect = await editMessage(questionMessage, {
                     title: "✅  Correct!",
                     level: MessageLevel.SUCCESS,
+                    footer: `Answered by: ${answers.first()!.author.username}`
                 });
-                await deleteMessages([alertCorrect, answers.first()!], 5000);
+                await deleteMessages([alertCorrect], 5000);
             }
 
+            // allow sending new questions
             this.messageLock = false;
         }
             // user didn't answer in time
         catch {
-            const alertOutOfTime = await message.channel.send({content: "You didn't answer in time"});
+            const alertOutOfTime = await editMessage(questionMessage, {
+                title: "❗  You ran out of time!",
+                level: MessageLevel.WARNING,
+                footer: `Correct answer: ${question.correctAnswer}`,
+            })
             await deleteMessages([questionMessage, alertOutOfTime], 5000);
+            this.messageLock = false;
         }
     }
 }
