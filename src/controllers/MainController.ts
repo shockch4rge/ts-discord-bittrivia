acd771e56d9b74bee0965de9d262bbf081abc9e9
Shockch4rge <favouriteteo@gmail.com>
2021-10-09 14:39:16 +0800

    Code cleanup

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -1,28 +1,22 @@
-import {Client, Message} from "discord.js";
-import {askQuestion} from "../../utils/messaging";
-import Question from "../models/Question";
-import AnswerCollector from "../AnswerCollector";
-import QuestionRequester from "../services/QuestionRequester";
+import {Client} from "discord.js";
 import MessageController from "./MessageController";
 
 export default class MainController {
-    private readonly bot: Client;
     private readonly messageController;
 
-    public constructor(bot: Client) {
-        this.bot = bot;
+    public constructor() {
         this.messageController = new MessageController();
     }
 
     public subscribeBotEvents(bot: Client) {
-        bot.on("ready", this.handleReady);
+        bot.once("ready", this.handleReady);
         bot.on("messageCreate", async message => {
             await this.messageController.handleMessageCreate(message);
         });
     }
 
     public handleReady(bot: Client) {
-        console.log(`${bot.user!.tag} is ready!`);
+        console.log(`${bot.user!.username} is ready!`);
         bot.user!.setPresence({
             activities: [{type: "LISTENING", name: "all messages!"}],
             status: "online",
