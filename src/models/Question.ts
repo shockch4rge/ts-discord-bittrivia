7051b9f2213d50ae3d39b4b75f2cfafa01504705
Shockch4rge <favouriteteo@gmail.com>
2021-10-09 14:37:56 +0800

    Checks answer in the form of string/number from user

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,6 +1,8 @@
 import {QuestionData} from "../../utils/types";
 import {shuffle} from "../../utils/utils";
 import he from "he";
+import {TextBasedChannels} from "discord.js";
+import {sendMessage} from "../../utils/messaging";
 
 export default class Question {
     public readonly content: string;
@@ -14,7 +16,9 @@ export default class Question {
     public constructor(data: QuestionData) {
         this.content = he.decode(data.question);
         this.correctAnswer = he.decode(data.correct_answer);
+        // decode every wrong answer
         this.wrongAnswers = data.incorrect_answers.map(answer => he.decode(answer));
+        // combine correct and wrong answers and shuffle them for randomness
         this.allAnswers = shuffle(this.wrongAnswers.concat(this.correctAnswer));
         this.category = he.decode(data.category);
         this.type = he.decode(data.type);
@@ -22,8 +26,19 @@ export default class Question {
     }
 
     // Check if the answer is correct
-    public checkCorrect(answer: number) {
+    public checkCorrect(answer: string) {
+        // get numeric form in case user decides to answer with number
+        const ans: number = Number.parseInt(answer) - 1;
         const correctIndex = this.allAnswers.indexOf(this.correctAnswer);
-        return !Number.isNaN(answer) && answer === correctIndex;
+        const correctString = this.correctAnswer.toLowerCase();
+
+        /*
+            In this case, the user can answer with either the index
+            or the literal string of the answer.
+         */
+        const isCorrectIndex = !Number.isNaN(ans) && ans === correctIndex;
+        const isCorrectString = new RegExp(correctString).test(answer);
+
+        return isCorrectIndex || isCorrectString;
     }
 }
