479f827e9931f43f659fdd565ba0a629142af51a
Shockch4rge <favouriteteo@gmail.com>
2021-10-28 20:14:03 +0800

    Troubleshooting

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
 import {Client, Intents} from 'discord.js';
-import MainController from "./controllers/MainController";
+import BotHelper from "./helpers/BotHelper";
 
 const bot = new Client({intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]});
-const mainController = new MainController();
-
-mainController.subscribeBotEvents(bot);
+const botHelper = new BotHelper(bot);
 
 void bot.login(process.env.BOT_TOKEN);
