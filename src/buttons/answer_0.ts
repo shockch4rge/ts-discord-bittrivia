8b57945605b97b18fbd653bf86567ae09a24f14e
Shockch4rge <favouriteteo@gmail.com>
2021-10-30 22:11:08 +0800

    Button files based on max number of answers per question

---
+++ src/buttons/answer_0.ts
@@ -0,0 +1,26 @@
+import { ButtonFile } from "../helpers/BotHelper";
+
+module.exports = {
+    id: "answer_0",
+    execute: async helper => {
+        // const question = helper.cache.getTriviaService().question!;
+        // const button = helper.interaction.component as MessageButton
+        // const answer = button.label!;
+        //
+        // console.log(answer);
+        //
+        // if (question.checkCorrect(answer)) {
+        //     await helper.respond("✅  You got the correct answer!");
+        //     await delay(3000);
+        //     await helper.interaction.deleteReply();
+        // }
+        // else {
+        //     await helper.respond("❌  dumbass that was the wrong answer");
+        //     await delay(1500);
+        //     await helper.interaction.deleteReply();
+        // }
+
+        // console.log("0 executed");
+    }
+
+} as ButtonFile;
