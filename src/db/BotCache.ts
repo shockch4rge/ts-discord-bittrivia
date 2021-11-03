0c08817a7666212fb8a10b19ad9d97ae4e479712
Shockch4rge <favouriteteo@gmail.com>
2021-11-03 17:57:59 +0800

    Connect to Firestore to hold player data

--- src/db/BotCache.ts
+++ src/db/BotCache.ts
@@ -1,26 +1,77 @@
 import GuildCache from "./GuildCache";
 import { Client, Collection, Guild } from "discord.js";
+import admin, { firestore } from "firebase-admin";
+import CollectionReference = firestore.CollectionReference;
+import DocumentData = firestore.DocumentData;
+
+const auth = require("../../auth.json");
 
 export default class BotCache {
-    public bot: Client;
-    public guilds: Collection<string, GuildCache>
+    private readonly db: FirebaseFirestore.Firestore;
+    public readonly bot: Client;
+    public readonly guildCaches: Collection<string, GuildCache>;
+    public readonly guildRefs: CollectionReference<DocumentData>;
 
     public constructor(bot: Client) {
+        // init db
+        admin.initializeApp({ credential: admin.credential.cert(auth.firebase.service_account) });
+        this.db = admin.firestore();
+
         this.bot = bot;
-        this.guilds = new Collection();
+        this.guildCaches = new Collection<string, GuildCache>();
+        this.guildRefs = this.db.collection("guilds");
     }
 
     public getGuildCache(guild: Guild): Promise<GuildCache> {
         return new Promise<GuildCache>((resolve, reject) => {
-            let cache = this.guilds.get(guild.id);
+            let cache = this.guildCaches.get(guild.id);
 
+            // guild doesn't exist locally; add it
             if (!cache) {
-                // guild doesn't exist locally; add it
-                cache = new GuildCache(this.bot, guild);
-                this.guilds.set(guild.id, cache);
-            }
+                const playerRefs = this.guildRefs
+                    .doc(guild.id)
+                    .collection("players");
+                cache = new GuildCache(this.bot, guild, playerRefs);
+                this.guildCaches.set(guild.id, cache);
 
-            resolve(cache);
+                this.guildRefs
+                    .doc(guild.id)
+                    .get()
+                    .then(snap => {
+                        if (!snap.exists) {
+                            reject();
+                        }
+                        else {
+                            resolve(cache!);
+                        }
+                    });
+
+            }
+            // guild exists
+            else {
+                resolve(cache);
+            }
         });
     }
+
+    public async createGuildCache(guild: Guild) {
+        const doc = await this.guildRefs.doc(guild.id).get();
+        if (!doc.exists) {
+            await this.guildRefs
+                .doc(guild.id)
+                .collection("members");
+            console.log("Created guild")
+        }
+        await this.getGuildCache(guild);
+    }
+
+    public async deleteGuildCache(guildId: string) {
+        const doc = await this.guildRefs.doc(guildId).get();
+        if (doc.exists) {
+            await this.guildRefs.doc(guildId).delete();
+        }
+        this.guildCaches.delete(guildId);
+
+    }
+
 }
