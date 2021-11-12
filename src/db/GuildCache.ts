f7a3995e895fb5225a67eed287202d712195cb78
Shockch4rge <favouriteteo@gmail.com>
2021-11-13 00:40:51 +0800

    Don't cache players in guild, query Firestore instead

--- src/db/GuildCache.ts
+++ src/db/GuildCache.ts
@@ -1,56 +1,46 @@
-import { Client, Collection, Guild, GuildMember } from "discord.js";
+import { Client, Guild, GuildMember } from "discord.js";
 import TriviaService from "../services/TriviaService";
-import Player from "../models/Player";
+import Player, { PlayerData } from "../models/Player";
 import { firestore } from "firebase-admin";
+import XpHelper from "../utilities/XpHelper";
 import CollectionReference = firestore.CollectionReference;
 import DocumentData = firestore.DocumentData;
+import FieldValue = firestore.FieldValue;
 
 export default class GuildCache {
     public readonly bot: Client;
     public readonly guild: Guild;
-    public readonly players: Collection<string, Player>;
     public readonly playerRefs: CollectionReference<DocumentData>;
     public readonly service: TriviaService;
 
     public constructor(bot: Client, guild: Guild, playerRefs: CollectionReference<DocumentData>) {
         this.bot = bot;
         this.guild = guild;
-        this.players = new Collection<string, Player>();
         this.playerRefs = playerRefs;
         this.service = new TriviaService();
-
-        this.cachePlayersFromDb();
-    }
-
-    private cachePlayersFromDb() {
-        this.playerRefs.get().then(snap => {
-            snap.forEach(doc => {
-                const member = this.guild.members.cache.get(doc.id);
-
-                if (member) {
-                    this.players.set(doc.id, new Player(member, {
-                        level: doc.get("level") ?? -1,
-                        xp: doc.get("xp") ?? -1,
-                        correct: doc.get("correct") ?? -1,
-                        wrong: doc.get("wrong") ?? -1,
-                    }));
-                }
-            });
-        });
     }
 
     /**
      * This method will always return a player, as they are derived from guild members.
-     * @param member
      */
-    public async getPlayer(member: GuildMember) {
-        let player = this.players.get(member.id);
+    public async getPlayerData(id: string) {
+        const playerRef = await this.playerRefs
+            .doc(id)
+            .get();
 
-        if (!player) {
-            player = await this.registerPlayer(member);
+        if (!playerRef.exists) {
+            return undefined;
         }
 
-        return player!;
+        return playerRef.data() as PlayerData;
+    }
+
+    public async awardXp(playerId: string) {
+        await this.playerRefs
+            .doc(playerId)
+            .update({
+                xp: FieldValue.increment(XpHelper.questionXp),
+            });
     }
 
     public async registerPlayer(member: GuildMember) {
@@ -60,10 +50,9 @@ export default class GuildCache {
             await this.playerRefs
                 .doc(member.id)
                 .create(Player.getEmptyData() as DocumentData);
-
-            const player = new Player(member, Player.getEmptyData());
-            this.players.set(player.member.id, player);
-            return player;
+        }
+        else {
+            throw new Error(`${member.id} already registered`);
         }
     }
 }
